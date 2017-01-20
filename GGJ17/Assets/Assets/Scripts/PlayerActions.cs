6791cc057a6c575c461208a2ec6dc50e9342232b
KaitoKid1111 <daleorei@gmail.com>
2017-01-20 23:33:10 +0100

    Player prefab/Scripts sobre el player/GameManager

---
+++ GGJ17/Assets/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+
+    public float m_playerSpeed = 5.0f;
+    public GameObject attack;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton(KeyCodes.Up))
+        {
+            transform.Translate(0, Time.deltaTime * m_playerSpeed, 0);
+        }
+
+        if (Input.GetButton(KeyCodes.Down))
+        {
+            transform.Translate(0, Time.deltaTime * -m_playerSpeed, 0);
+        }
+
+        if (Input.GetButton(KeyCodes.Left))
+        {
+            transform.Translate(Time.deltaTime * -m_playerSpeed, 0, 0);
+        }
+
+        if (Input.GetButton(KeyCodes.Right))
+        {
+            transform.Translate(Time.deltaTime * m_playerSpeed, 0, 0);
+        }
+
+        if(Input.GetButtonDown(KeyCodes.Attack) && !attack.activeInHierarchy) //&& Comprobar si el ataque anterior ha acabado)
+        {
+            attack.SetActive(true);
+        }
+    }
+}
---
+++ GGJ17/Assets/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    public int life;
+    public 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(life == 0)
+        {
+            death();
+        }
+	}
+
+    public void death()
+    {
+        //gameManager -> game over
+    }
+    public void damage()
+    {
+        --life;
+    }
+}
