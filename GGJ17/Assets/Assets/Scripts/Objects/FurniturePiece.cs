5df4086bda2ac172ed9c9e83efb64076340b6e53
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2017-01-21 20:13:38 +0100

    subida de

--- GGJ17/Assets/Assets/Scripts/Objects/FurniturePiece.cs
+++ GGJ17/Assets/Assets/Scripts/Objects/FurniturePiece.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class FurniturePiece : MonoBehaviour {
 
+    public bool isCabinet = false;
 
     public bool emisorPlace = false;
     public int x;
     public int y;
+    private bool alreadyBroken = false;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (isCabinet && !alreadyBroken)
+        {
+            if(coll.CompareTag(KeyCodes.PlayerAttack))
+            {
+                Debug.Log("Playerattac");
+                GetComponent<Animator>().enabled = true;
+                alreadyBroken = true;
+            }
+        }
+    }
 }
