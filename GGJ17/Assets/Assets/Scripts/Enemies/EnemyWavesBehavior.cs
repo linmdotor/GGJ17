dc92e4d5a1a01f60c785090ad23413c0e93100f2
Lin M. Dotor <linmdotor@gmail.com>
2017-01-21 15:16:50 +0100

    Añadido random para el comienzo de las ondas de enemigos

--- GGJ17/Assets/Assets/Scripts/Enemies/EnemyWavesBehavior.cs
+++ GGJ17/Assets/Assets/Scripts/Enemies/EnemyWavesBehavior.cs
@@ -6,24 +6,30 @@ public class EnemyWavesBehavior : MonoBehaviour {
 	public float weaponTimeOn;
 	public float weaponTimeOff;
 
-	public WaveEffect[] waves;
-
-	private float currentTime = 0f;
+	public float randomDelayedStartTime = 5f;
 	public bool startEnabled = false;
 	private bool weaponEnabled;
 
+	public WaveEffect[] waves;
+
+	private float currentTime = 0f;
+	
+
 	// Use this for initialization
 	void Start () {
-		currentTime = 0f;
 		weaponEnabled = startEnabled;
 		SetWeaponEnabled(startEnabled);
-	}
+		randomDelayedStartTime *= Random.value;
+		//Hack to start delayed
+		currentTime = -randomDelayedStartTime;
+		Debug.Log("RAND: " + randomDelayedStartTime);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
 		currentTime += Time.deltaTime;
-
+		Debug.Log("TIME: " + currentTime);
 		if(weaponEnabled && currentTime >= weaponTimeOn)
 		{
 			weaponEnabled = false;
