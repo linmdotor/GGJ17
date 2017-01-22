fa67044efd10d14369996dae5b9b9278b7b4ba1b
Iulen-PC\Iulen <im_993@hotmail.com>
2017-01-22 10:01:23 +0100

    Iulen tiene un poco de retraso pero se le quiere

--- GGJ17/Assets/Assets/Scripts/UIManager.cs
+++ GGJ17/Assets/Assets/Scripts/UIManager.cs
@@ -67,7 +67,7 @@ public class UIManager : MonoBehaviour {
     {
         scoreText.text = "Score: " + score;
         deadMenuScoreText.text = "Score: " + score;
-        winMenuScoreText.text = "Coreo: " + score;
+        winMenuScoreText.text = "Score: " + score;
     }
 
     public void changeLifeText(int life)
