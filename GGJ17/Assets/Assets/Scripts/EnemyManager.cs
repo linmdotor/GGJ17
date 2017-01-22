db5e0780966052430b6e6ad30cce44e460aaf517
KaitoKid1111 <daleorei@gmail.com>
2017-01-22 01:13:03 +0100

    EnemyManager, spawn y cosas tío. Ahora que lo pienso, Lin no ha salido en la última foto illo y en realidad me da tor coraje porque illo, hubiera sío una foto to guapa pero en plan "Colega vaya jetos de destrozaos que calzamos". Total, que la crema de verduras tampoco era pa tanto, pero por lo menos quitaba el hambre.

---
+++ GGJ17/Assets/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+    public int numberEnemies = 10;
+
+    public GameObject enemyCameraPrefab;
+    public GameObject enemyEmissorPrefab;
+    public GameObject enemyHeadphonesPrefab;
+
+    public enum enemyPrefabs
+    {
+        enemyCameraPrefab,
+        enemyEmissorPrefab,
+        enemyHeadphonesPrefab
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void loadEnemies()
+    {
+        MapTile[] freeSlots = MapManager.MapManagerInstance.GetMapTiles(MapTile.TileType.Floor);
+        for (int enemies = 0; enemies < numberEnemies; ++enemies)
+        {
+            MapTile freeSlot = freeSlots[Random.Range(0, freeSlots.Length)];
+            //SI HAY PROBLEMA AÑADIR CONTROL DE REPETICIÓN DE SPAWN EN MISMA CASILLA.
+            Vector3 spawnPos = new Vector3(freeSlot.transform.position.x, freeSlot.transform.position.y, 0);
+            EnemyManager.enemyPrefabs enemy = (EnemyManager.enemyPrefabs)Random.Range(0, 2);
+            
+            switch(enemy)
+            {
+                case enemyPrefabs.enemyCameraPrefab:
+                    GameObject.Instantiate(enemyCameraPrefab, spawnPos, Quaternion.identity);
+                    break;
+                case enemyPrefabs.enemyEmissorPrefab:
+                    GameObject.Instantiate(enemyEmissorPrefab, spawnPos, Quaternion.identity);
+                    break;
+                case enemyPrefabs.enemyHeadphonesPrefab:
+                    GameObject.Instantiate(enemyHeadphonesPrefab, spawnPos, Quaternion.identity);
+                    break;
+            }
+        }
+    }
+}
