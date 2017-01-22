6ee8cfe830b757fad939e07a43bd0087359c5681
Iulen-PC\Iulen <im_993@hotmail.com>
2017-01-22 11:40:21 +0100

    BLOOOOOOOOOOOOOOOOOOOOOOOOOOOOD

---
+++ GGJ17/Assets/Assets/Scripts/Blood.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood : MonoBehaviour {
+
+    [SerializeField]
+    public Sprite[] bloodSprites;
+
+	// Use this for initialization
+	void Start () {
+
+        this.GetComponent<SpriteRenderer>().sprite = bloodSprites[Random.Range(0,bloodSprites.Length)];
+
+	}
+
+}
